018af75f1c91ab9dec5b8d988815e552a100440e
Dreaming-J <kimjjong321@naver.com>
2023-12-04 16:26:07 +0900

    :white_check_mark: test. 코치 이름 설정

---
+++ src/test/java/menu/model/CoachTest.java
@@ -0,0 +1,18 @@
+package menu.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Test;
+
+public class CoachTest {
+
+    @Test
+    void 이름_테스트() {
+        assertThatThrownBy(() -> new Coach("김"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Coach("김수한무두"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertDoesNotThrow(() -> new Coach("김수한무"));
+    }
+}
