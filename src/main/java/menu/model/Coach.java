068310254a6e4e978d36ef48e4825e84cdabc725
Dreaming-J <kimjjong321@naver.com>
2023-12-04 17:54:09 +0900

    :recycle: refactor. 못 먹는 음식 HateFood로 객체 분리

--- src/main/java/menu/model/Coach.java
+++ src/main/java/menu/model/Coach.java
@@ -1,24 +1,13 @@
 package menu.model;
 
 import static menu.message.ErrorMsg.COACH_NAME_ERROR;
-import static menu.message.ErrorMsg.HATE_FOOD_ERROR;
-import static menu.util.Constant.COMMA;
-import static menu.util.Constant.HATE_FOOD_REGEX;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class Coach {
 
-    private static final int MIN_INCLUSIVE_OF_NAME = 2;
-    private static final int MAX_INCLUSIVE_OF_NAME = 4;
-    private static final int MAX_INCLUSIVE_OF_HATE_FOOD = 2;
-    private static final int DUPLICATED_NUMBER = 1;
+    private static final int MIN_INCLUSIVE = 2;
+    private static final int MAX_INCLUSIVE = 4;
     private final String name;
-    private List<Food> hateFood;
+    private HateFood hateFood;
 
     public Coach(String name) {
         validateName(name);
@@ -26,33 +15,12 @@ public class Coach {
     }
 
     private void validateName(String name) {
-        if (name.length() < MIN_INCLUSIVE_OF_NAME || name.length() > MAX_INCLUSIVE_OF_NAME) {
+        if (name.length() < MIN_INCLUSIVE || name.length() > MAX_INCLUSIVE) {
             throw new IllegalArgumentException(COACH_NAME_ERROR.toString());
         }
     }
 
-    public void setHateFood(String hateFood) {
-        if (hateFood.isEmpty()) {
-            this.hateFood = new ArrayList<>();
-            return;
-        }
-
-        validateHateFood(hateFood);
-        this.hateFood = Stream.of(hateFood.split(COMMA))
-                .map(Food::new)
-                .toList();
-        validateHateFood(this.hateFood);
-    }
-
-    private void validateHateFood(String hateFood) {
-        if (!Pattern.matches(HATE_FOOD_REGEX, hateFood)) {
-            throw new IllegalArgumentException(HATE_FOOD_ERROR.toString());
-        }
-    }
-
-    private void validateHateFood(List<Food> hateFood) {
-        if (hateFood.size() == MAX_INCLUSIVE_OF_HATE_FOOD & Set.copyOf(hateFood).size() == DUPLICATED_NUMBER) {
-            throw new IllegalArgumentException(HATE_FOOD_ERROR.toString());
-        }
+    public void createHateFood(String hateFood) {
+        this.hateFood = new HateFood(hateFood);
     }
 }
---
+++ src/main/java/menu/model/HateFood.java
@@ -0,0 +1,46 @@
+package menu.model;
+
+import static menu.message.ErrorMsg.HATE_FOOD_ERROR;
+import static menu.util.Constant.COMMA;
+import static menu.util.Constant.HATE_FOOD_REGEX;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class HateFood {
+
+    private static final int MAX_INCLUSIVE = 2;
+    private static final int DUPLICATED_NUMBER = 1;
+    private final List<Food> hateFood;
+
+    public HateFood(String hateFood) {
+        this.hateFood = setHateFood(hateFood);
+        validateHateFood(this.hateFood);
+    }
+
+    private List<Food> setHateFood(String hateFood) {
+        if (hateFood.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        validateHateFood(hateFood);
+        return Stream.of(hateFood.split(COMMA))
+                .map(Food::new)
+                .toList();
+    }
+
+    private void validateHateFood(String hateFood) {
+        if (!Pattern.matches(HATE_FOOD_REGEX, hateFood)) {
+            throw new IllegalArgumentException(HATE_FOOD_ERROR.toString());
+        }
+    }
+
+    private void validateHateFood(List<Food> hateFood) {
+        if (hateFood.size() == MAX_INCLUSIVE & Set.copyOf(hateFood).size() == DUPLICATED_NUMBER) {
+            throw new IllegalArgumentException(HATE_FOOD_ERROR.toString());
+        }
+    }
+}
